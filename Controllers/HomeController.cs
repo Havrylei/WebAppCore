42cf96bf8aeaf2a60c31d9ad6586d32dab826889
Ubuntu <Petro@WebService.r1t1gs2nxajejhgsshbjvmv4pf.bx.internal.cloudapp.net>
2020-06-15 09:06:33 +0000

    added base

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppCore.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("It works");
+        }
+    }
+}
